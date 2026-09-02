0a38f33a517e154a9a416534fcbffaaf1e2b3a98
Nattha-nan <natthanan.bo@kkumail.com>
2026-09-02 14:17:59 +0700

    file

---
+++ lab8_6733800371_sec1/src/main/java/com/example/lab8_6733800371_sec1/repository/ProductRepository.java
@@ -0,0 +1,5 @@
+package com.example.lab8_6733800371_sec1.repository;
+
+public class ProductRepository {
+    
+}
---
+++ lab8_6733800371_sec1/src/main/java/com/example/lab8_6733800371_sec1/strategy/NoDiscountStrategy.java
@@ -0,0 +1,5 @@
+package com.example.lab8_6733800371_sec1.strategy;
+
+public class NoDiscountStrategy {
+    
+}
