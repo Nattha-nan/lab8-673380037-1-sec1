75639309e10d8f5a236de730f0e566a385bf2ec7
Nattha-nan <natthanan.bo@kkumail.com>
2026-09-02 13:56:25 +0700

    code

---
+++ lab8_6733800371_sec1/src/main/java/com/example/lab8_6733800371_sec1/Lab86733800371Sec1Application.java
@@ -0,0 +1,13 @@
+package com.example.lab8_6733800371_sec1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab86733800371Sec1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab86733800371Sec1Application.class, args);
+	}
+
+}
---
+++ lab8_6733800371_sec1/src/test/java/com/example/lab8_6733800371_sec1/Lab86733800371Sec1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8_6733800371_sec1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab86733800371Sec1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
